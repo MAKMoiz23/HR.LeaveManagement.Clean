3dab56ee0072408c95b17f80ac1ad8a24f551145
Moiz Khan <moiz.khan@shispare.com>
2024-07-22 01:44:41 +0500

    .

---
+++ HR.LeavManagement.Application.UnitTests/Mocks/MockLeaveTypeRepository.cs
@@ -0,0 +1,44 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Domain;
+using Moq;
+
+namespace HR.LeavManagement.Application.UnitTests.Mocks
+{
+    public class MockLeaveTypeRepository
+    {
+        public static Mock<ILeaveTypeRepository> GetMockLeaveTypes()
+        {
+            List<LeaveType> leaveTypes = new List<LeaveType> {
+                new LeaveType {
+                    Id = 1,
+                    Name = "Test Vacations",
+                    DefaultDays = 10,
+                },new LeaveType {
+                    Id = 2,
+                    Name = "Test Sick",
+                    DefaultDays = 6,
+                },new LeaveType {
+                    Id = 1,
+                    Name = "Test Casual",
+                    DefaultDays = 18,
+                }
+            };
+
+            var mockRepo = new Mock<ILeaveTypeRepository>();
+
+            mockRepo
+                .Setup(r => r.GetAsync())
+                .ReturnsAsync(leaveTypes);
+
+            mockRepo
+                .Setup(r => r.CreateAsync(It.IsAny<LeaveType>()))
+                .Returns((LeaveType leaveType) =>
+                {
+                    leaveTypes.Add(leaveType);
+                    return Task.CompletedTask;
+                });
+
+            return mockRepo;
+        }
+    }
+}
